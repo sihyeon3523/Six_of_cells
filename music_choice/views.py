815a79500188182384b618efd7519170f95bbeca
SooJeong Heo <78069770+sdoubleoj@users.noreply.github.com>
2022-05-30 10:20:06 +0900

    Update views.py

--- music_choice/views.py
+++ music_choice/views.py
@@ -10,20 +10,29 @@ def music_choice(request):
     sadness = pd.read_csv("./data/sadness.csv", encoding='utf-8', lineterminator='\n')
     surprise = pd.read_csv("./data/surprise.csv", encoding='utf-8', lineterminator='\n')
 
+    happiness = happiness[['Title','Artist']]
+    anger = anger[['Title','Artist']]
+    sadness = sadness[['Title','Artist']]
+    surprise = surprise[['Title','Artist']]
+
     #비복원 추출
     hap_2 = happiness.sample(n=2, replace=False)
     ang_2 = anger.sample(n=2, replace=False)
     sad_2 = sadness.sample(n=2, replace=False)
     sur_2 = surprise.sample(n=2, replace=False)
 
-    #랜덤추출한 곡을 담은 df
+    #랜덤추출한 곡 담은 df
     random_songs_df = pd.concat([hap_2,ang_2,sad_2,sur_2])
 
-    context = {'random_songs_df':random_songs_df}
+    random_songs_list = []
+    for row in random_songs_df.iterrows():
+    #     print(row[1][0]) #제목
+    #     print(row[1][1]) #가수
 
-    # for row in random_songs_df.iterrows():
-    #     title = row[1][2] #제목
-    #     artist = row[1][3] #가수
+        random_songs_list.append((row[1][0],row[1][1]))
+
+    context = {'random_songs_list':random_songs_list}
+
 
     return render(request, 'music_choice/music_choice.html', context)
 
